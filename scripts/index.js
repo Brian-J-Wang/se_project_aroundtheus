2cba8edaea21886b9b20b12411621d0258304458
Brian Wang <brianwang7102@gmail.com>
2024-02-19 05:58:33 -0500

    fix: issues in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-initialCards = [
+let initialCards = [
     {
         name: "New York",
         link: "https://plus.unsplash.com/premium_photo-1681803606255-cb66b02f2b56?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
