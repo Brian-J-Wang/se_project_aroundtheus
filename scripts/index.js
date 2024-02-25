01b477cbdc2e84bd3c02738abf158bc59c6a1ba4
Brian Wang <brianwang7102@gmail.com>
2024-02-25 13:59:15 -0500

    fix some issus with index.js, correct modal window when screen width: 320px

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
     {
         name: "New York",
         link: "https://plus.unsplash.com/premium_photo-1681803606255-cb66b02f2b56?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -27,45 +27,49 @@ let initialCards = [
 const places = document.querySelector(".places");
 const cardTemplate = document.querySelector("#card").content;
 for (let i = 0; i < initialCards.length; i++) {
-    let cardCopy = cardTemplate.cloneNode(true);
+    let cardCopy = CreateCard(cardTemplate);
     cardCopy.querySelector('.places__image').src = initialCards[i].link;
+    cardCopy.querySelector('.places__image').alt = "place image"
     cardCopy.querySelector('.places__name').textContent = initialCards[i].name;
 
     places.append(cardCopy);
 }
 
+function CreateCard(template) {
+    return template.cloneNode(true);
+}
+
 
 const modal = document.querySelector('.modal');
-
-const InputName = modal.querySelector('.modal__input[name=name]');
+const inputName = modal.querySelector('.modal__input[name=name]');
 const profileName = document.querySelector('.profile__name'); 
-const InputDesc = modal.querySelector('.modal__input[name=desc]');
+const inputDesc = modal.querySelector('.modal__input[name=desc]');
 const profileDesc = document.querySelector('.profile__desc');
 
 const editButton = document.querySelector('.profile__edit');
-editButton.addEventListener("click", onEditClicked);
-function onEditClicked() {
-    InputName.value = profileName.textContent;
-    InputDesc.value = profileDesc.textContent;
-    modal.classList.add('modal__show');
+editButton.addEventListener("click", openProfileModal);
+function openProfileModal() {
+    inputName.value = profileName.textContent;
+    inputDesc.value = profileDesc.textContent;
+    modal.classList.add('modal_opened');
 }
 
 const closeModalWindow = document.querySelector(".modal__close");
-closeModalWindow.addEventListener("click", onFormClosed);
-function onFormClosed() {
-    modal.classList.remove("modal__show");
+closeModalWindow.addEventListener("click", closeProfileModal);
+function closeProfileModal() {
+    modal.classList.remove("modal_opened");
 }
 
 const modalForm = modal.querySelector('.modal__form');
 modalForm.addEventListener("submit", function(evt) {
     evt.preventDefault();
-    onEditSaved();
+    saveProfileData();
 });
 
-function onEditSaved() {
-    profileName.textContent = InputName.value;
-    profileDesc.textContent = InputDesc.value;
-    onFormClosed();
+function saveProfileData() {
+    profileName.textContent = inputName.value;
+    profileDesc.textContent = inputDesc.value;
+    closeProfileModal();
 }
 
 
